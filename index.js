ae8fdd658f8830f8b10298be6a74e0a11351312e
tgsmith-17 <gagesmith369@gmail.com>
2021-09-24 17:52:34 -0400

    Slowly but surely

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-let userName = "bouchard";
-
 let getUser = prompt("Username:");
 
-if(userName != getUser || getUser == '') {
-    window.location.replace("https://dood.al/pinktrombone/");
+let user = '';
+
+if(getUser == "bouchard") {
+    user = "Dr. Bouchard";
 }
+else if(getUser == "tgsmith") {
+    user = "Gage";
+}
+else {
+    let choice = Number(prompt("Incorrect Username\n1 or 2?"));
+    if(choice == 1) {
+        window.location.replace("https://dood.al/pinktrombone/");
+    }
+    else if(choice == 2) {
+        window.location.replace("https://www.adultswim.com/etcetera/elastic-man/app.html");
+    }
+}
+
+let showingUser = document.getElementById("user");
+
+if(user == "Dr. Bouchard") {
+showingUser.style.color = "chartreuse";
+}
+
+showingUser.innerHTML = "Welcome, " + user;
